234e6e47b1d9f200d5b596ca9b954d75b1d0582a
dgtdsyyy <114586786+dgtdsyyy@users.noreply.github.com>
2022-10-09 16:18:56 +0700

    Add files via upload

---
+++ tweak.js
@@ -0,0 +1,36 @@
+console.log("Starting Tweak")
+const intro = document.getElementById('product1')
+const wrapper = document.getElementById('wrapper')
+const newBox = document.getElementById('newBox')
+const productBox1 = document.getElementById('productBox1')
+const productBox2 = document.getElementById('productBox2')
+const productBox3 = document.getElementById('productBox3')
+const productBox4 = document.getElementById('productBox4')
+const productBox5 = document.getElementById('productBox5')
+const productBox6 = document.getElementById('productBox6')
+
+function getDetail(nomor) {
+    console.log('product: ', nomor)
+    newBox.style.display = 'block'
+    wrapper.style.display = 'none'
+
+    if (nomor === 1) {
+        productBox1.style.display = 'flex'
+    } else if (nomor === 2) {
+        productBox2.style.display = 'flex'
+    } else if (nomor === 3) {
+        productBox3.style.display = 'flex'
+    } else if (nomor === 4) {
+        productBox4.style.display = 'flex'
+    } else if (nomor === 5) {
+        productBox5.style.display = 'flex'
+    } else if (nomor === 6) {
+        productBox6.style.display = 'flex'
+    } 
+}
+
+// function backHome() {
+//     newBox.style.display = 'none';
+//     wrapper.style.display = 'block';
+// }
+
